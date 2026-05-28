4130d28458444063a24d2f71486cc1246ec4d4b4
boudy2001 <abdelrahmanali345@gmail.com>
2026-05-28 17:53:50 +0300

    Add opportunity analysis module init

---
+++ opportunity_analysis/__init__.py
@@ -0,0 +1,8 @@
+"""Opportunity Analysis Module - Lost opportunity post-mortem AI analysis"""
+
+__version__ = "0.1.0"
+
+from .analyzer import OpportunityAnalyzer
+from .models import LossReport
+
+__all__ = ["OpportunityAnalyzer", "LossReport"]
